29c0a79bf1776df27d360a18555c737deb425e40
Ran Tavory <rantav@gmail.com>
2013-01-04 00:08:49 +0200

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+if [ $# -ne 1 ]
+then
+  echo "Usage:"
+  echo "       `basename $0` 'commit message'"
+  exit 1
+fi
+
+set -e
+yeoman clean
+yeoman build
+# this looks like a bug (in confess.js or in yeoman?) but the urls of the images are with a hostname and that's bad
+sed -i.bak 's/http:\/\/localhost:3501//g' dist/manifest.appcache
+rm dist/manifest.appcache.bak
+echo "# git rev: `git rev-parse HEAD`" >> dist/manifest.appcache
+echo "<!-- Built on `date`,     git rev: `git rev-parse HEAD` -->" >> dist/index.html
+git add .
+set +e
+git ci -am "$1"
+set -e
+git co gh-pages
+[[ -e dist/dist ]] && rm dist/dist
+cp -r dist/* .
+git pl
+set +e
+git add .
+git ci -am "$1"
+set -e
+git push origin gh-pages
+git co master
